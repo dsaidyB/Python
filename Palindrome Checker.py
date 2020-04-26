6099dc471e161d3562aa7efbc068e6b38ae3cbb4
dsaidyB <64372567+dsaidyB@users.noreply.github.com>
2020-04-26 14:54:08 -0400

    Update Palindrome Checker.py

--- Palindrome Checker.py
+++ Palindrome Checker.py
@@ -1,25 +1,25 @@
 def palindrome (word):
     word = word.split(" ")
-    #print(word)
-    
+
     if (len(word) > 1):
-        for x in range(1,len(word),2):
-            hey = word[x-1] + word[x] 
+        for x in range(1,len(word)):
+            word[0] += word[x]
         
-        flip = hey[::-1]
+        flip = word[0][::-1]
 
     else:
         flip = word[0][::-1]
-    
+
     if(flip == word[0]):
         print("yes")
-        
+
     else:
         print("no")
         
-    #print(word)    
-    #print(flip)
-    
+
 palindrome("nurses run")
-palindrome(“racecar”)
-palindrome(“hello”)
+#yes
+palindrome("racecar")
+#yes
+palindrome("hello")
+#no
